960508e5433d7ce895a426a75161327db2147e7b
Leandro De Araujo <leandro.arj@outlook.com>
2021-09-20 20:51:42 +0100

    run.py app

---
+++ run.py
@@ -0,0 +1,9 @@
+import os
+from app import app
+
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5001")),
+        debug=True
+    )
