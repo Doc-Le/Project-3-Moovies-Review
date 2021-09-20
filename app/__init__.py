fe661afbc015f1977cd17fcec8bd5a6a6295fca1
Leandro De Araujo <leandro.arj@outlook.com>
2021-09-20 20:44:25 +0100

    pymongo application

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_pymongo  import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
+
+mongo = PyMongo(app)
+
+from app import home
+# from app import api
