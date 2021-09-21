327e79dc7207eea609e5c658684b112d373c4c93
Doc-Le <leandro.arj@outlook.com>
2021-09-21 09:46:58 +0100

    disable mongo connection, add static data

--- app/home.py
+++ app/home.py
@@ -1,9 +1,16 @@
 import logging
-from app import (app, mongo)
+# from app import (app, mongo)
+from app import app
 from flask import render_template
 
 @app.route("/")
 def main_page():
-    movies = mongo.db.movies.find({"year": 2021})
+    # movies = mongo.db.movies.find({"year": 2021})
+    movies=[
+        { "name": "Movie 1", "year": 2018 },
+        { "name": "Movie 2", "year": 2019 },
+        { "name": "Movie 3", "year": 2020 },
+        { "name": "Movie 4", "year": 2021 }
+    ]
     logging.info(movies)
     return render_template("index.html", movies=movies)
