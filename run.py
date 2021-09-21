8d161083d23ea369280ff964ab47d3596e5a4b2b
Doc-Le <leandro.arj@outlook.com>
2021-09-21 09:45:47 +0100

    remove development options

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5001")),
-        debug=True
-    )
+    app.run()
