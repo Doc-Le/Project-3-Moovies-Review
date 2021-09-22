fa4c9efa64eb5acfb5355ae273dbf8b939bb4056
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2021-09-22 19:28:40 +0100

    Fix environment variables misspelling

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 
-IP = os.environ.get("IP", app.config["FLASK_RUN_HOST"])
-PORT = int(os.environ.get("PORT", app.config["FLASK_RUN_PORT"]))
+IP = os.environ.get("IP", app.config["IP"])
+PORT = int(os.environ.get("PORT", app.config["PORT"]))
 
 
 if __name__ == '__main__':
