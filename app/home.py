8a82a0e21195a77ff8e54a391e11dd8f478d6592
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2021-09-22 20:16:16 +0100

    Enable get movies from DB

--- app/home.py
+++ app/home.py
@@ -5,12 +5,12 @@ from flask import render_template
 
 @app.route("/")
 def main_page():
-    # movies = mongo.db.movies.find({"year": 2021})
-    movies=[
-        { "name": "Movie 1", "year": 2018 },
-        { "name": "Movie 2", "year": 2019 },
-        { "name": "Movie 3", "year": 2020 },
-        { "name": "Movie 4", "year": 2021 }
-    ]
+    movies = mongo.db.movies.find({})
+#     movies=[
+#         { "name": "Movie 1", "year": 2018 },
+#         { "name": "Movie 2", "year": 2019 },
+#         { "name": "Movie 3", "year": 2020 },
+#         { "name": "Movie 4", "year": 2021 }
+#     ]
     logging.info(movies)
     return render_template("index.html", movies=movies)
