af5414731f9531f15ea6b3aeddd2e3a2f5e27922
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2021-09-22 19:25:07 +0100

    Update Flask ip and port environment variables

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from app import app
 
+IP = os.environ.get("IP", app.config["FLASK_RUN_HOST"])
+PORT = int(os.environ.get("PORT", app.config["FLASK_RUN_PORT"]))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host=IP,
+        port=PORT,
+        debug=True
+    )
