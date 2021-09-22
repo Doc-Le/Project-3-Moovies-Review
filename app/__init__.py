c002ea367ea76147a63c282aa4f08607c4c6de65
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2021-09-22 20:10:14 +0100

    Enable MongoDB Atlas connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
-# from flask_pymongo  import PyMongo
+from flask_pymongo  import PyMongo
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
+app.config['MONGO_URI'] = IP = os.environ.get("MONGO_URI", "mongodb://localhost:27017/test")
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 from app import home
 # from app import api
