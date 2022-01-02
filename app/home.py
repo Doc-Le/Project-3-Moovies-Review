5e4a91dea9a2568c5c0848f9e8c7ddccd5e1a66c
Leandro De Araujo <leandro.arj@outlook.com>
2022-01-02 16:34:23 +0000

    add @app route

--- app/home.py
+++ app/home.py
@@ -1,16 +1,29 @@
+import json
 import logging
+
+from bson import json_util
 from app import (app, mongo)
 from app import app
+from bson.objectid import ObjectId
 from flask import render_template
 
 @app.route("/")
 def main_page():
-    return render_template("index.html", movies=[])
+    movies = mongo.db.movies.find()
+    return render_template("index.html", movies=movies)
 
 @app.route("/movies")
 def get_movies():
     cursor = mongo.db.movies.find()
-    movies = [doc.to_json() for doc in cursor]
+    movies = json.loads(json_util.dumps(cursor))
     return {
         "movies": movies,
+    }
+
+@app.route("/movies/<id>")
+def get_movie_id(id):
+    cursor = mongo.db.movies.find_one({ "_id": ObjectId(id) })
+    movie = json.loads(json_util.dumps(cursor))
+    return {
+        "movie": movie,
     }
