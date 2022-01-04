f819f9b0c15ff358b4263bd6b4a70a54e4c3d716
Leandro De Araujo <leandro.arj@outlook.com>
2022-01-04 12:14:47 +0000

    add api url base

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 import os
 from flask import Flask
 from flask_pymongo  import PyMongo
+# from tmdb import create_session
 
 app = Flask(__name__)
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/test")
 
+MOVIE_API_BASE_URL ="https://api.themoviedb.org/3/movie/{}?api_key={}"
+MOVIE_API_KEY = os.environ.get("MOVIE_API_KEY", "30d7f1144aa1d0fce3d64afb43bb9a61")
+
+# comments about mongodb connect
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
 
+# comments about movie external api connect
+# create_session(API_KEY)
+#  import tmdb.py
+
 from app import home
-# from app import api
