9c154b3e51f3e97b692fff061fe391f9e18484a5
Leandro De Araujo <leandro.arj@outlook.com>
2022-01-04 19:45:37 +0000

    add tmdb.py

--- app/tmdb.py
+++ app/tmdb.py
@@ -1,5 +1,19 @@
+from . import MOVIE_API_KEY
+import tmdbsimple as tmdb
+from .models import (
+    Movie
+)
 
-class tmdb:
-    tmdb = {}
-    def create_session():
-        return self.tmdb
+tmdb.API_KEY = MOVIE_API_KEY
+tmdb.REQUESTS_TIMEOUT = (2, 5)
+
+def search_movies(query):
+    """ search movie in api """ 
+    search = tmdb.Search()
+    response = search.movie(query=query)
+    return search.results
+
+def get_movie(id):
+    """ search movie in api """
+    movie = tmdb.Movies(id)
+    return movie.info()
