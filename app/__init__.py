c3ca65ada2ba4a13b4a6c35fd30ab7e3233baf87
Leandro De Araujo <leandro.arj@outlook.com>
2022-01-07 18:30:09 +0000

    add comments

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app.secret_key = os.urandom(24)
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/test")
 MOVIE_API_KEY = os.environ.get("MOVIE_API_KEY", "30d7f1144aa1d0fce3d64afb43bb9a61")
 
-# comments about mongodb connect
+# Config connect with Mongodb
 app.config['MONGO_URI'] = MONGO_URI
 mongo = PyMongo(app)
 
