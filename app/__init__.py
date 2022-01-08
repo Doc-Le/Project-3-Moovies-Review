67be7766cb0a1f1ba5221a5c41c9a76c81589677
Leandro De Araujo <leandro.arj@outlook.com>
2022-01-08 21:27:26 +0000

    fixed movie api key

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 from flask_pymongo  import PyMongo
 
-
+# Config flask/mongodn locahost
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/test")
-MOVIE_API_KEY = os.environ.get("MOVIE_API_KEY", "30d7f1144aa1d0fce3d64afb43bb9a61")
+MOVIE_API_KEY = os.environ.get("MOVIE_API_KEY", "")
 
 # Config connect with Mongodb
 app.config['MONGO_URI'] = MONGO_URI
